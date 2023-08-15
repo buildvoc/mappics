2e51c9e3b17149386c861254fd1adfb97ed8db6c
chintan panchal <ckpanchal222@gmail.com>
2023-08-15 22:58:58 +0530

    Fix auto increment issue in buildings

--- src/Infrastructure/Migration/Version20230815172127.php
+++ src/Infrastructure/Migration/Version20230815172127.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230812173033 extends AbstractMigration
+final class Version20230815172127 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20230812173033 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE buildings (id VARCHAR(255) NOT NULL, os_topo_toid VARCHAR(255) DEFAULT NULL, os_topo_toid_version INT DEFAULT NULL, bha_process_date DATE DEFAULT NULL, tile_ref VARCHAR(255) DEFAULT NULL, abs_min VARCHAR(255) DEFAULT NULL, abs_h2 VARCHAR(255) DEFAULT NULL, abs_hmax VARCHAR(255) DEFAULT NULL, rel_h2 VARCHAR(255) DEFAULT NULL, rel_hmax VARCHAR(255) DEFAULT NULL, bha_conf VARCHAR(255) DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE buildings (id INT AUTO_INCREMENT NOT NULL, os_topo_toid VARCHAR(255) DEFAULT NULL, os_topo_toid_version INT DEFAULT NULL, bha_process_date DATE DEFAULT NULL, tile_ref VARCHAR(255) DEFAULT NULL, abs_min VARCHAR(255) DEFAULT NULL, abs_h2 VARCHAR(255) DEFAULT NULL, abs_hmax VARCHAR(255) DEFAULT NULL, rel_h2 VARCHAR(255) DEFAULT NULL, rel_hmax VARCHAR(255) DEFAULT NULL, bha_conf VARCHAR(255) DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
